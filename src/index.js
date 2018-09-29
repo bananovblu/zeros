9f9c6021343381e2dac0a5a59b366d77af349ea0
Yauheni Baranau <barapcmax@gmail.com>
2018-09-29 09:55:44 +0300

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  result = 0
+   for (var i = 1; i <= number; i++) {
+       x = i
+       while (x % 5 == 0){ 
+             x /= 5
+             result++
+       }
+   }
+   return result
 }
